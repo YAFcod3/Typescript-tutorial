8e5e0f8a0d14fe45ceadadeb2066cd1caffcc9d9
YAFcod3 <yasmaniarmas1995@gmail.com>
2023-06-24 23:03:18 -0400

    adv

--- react/src/app/layout.tsx
+++ react/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/context/ThemeContext";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Lama Dev TypeScript Tutorial",
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body >
         <ThemeProvider>
           <div className="container">
             <Navbar />
